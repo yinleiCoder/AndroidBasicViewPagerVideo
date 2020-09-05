8262e0b2fe7f3558a38c25abb039ee7b93d2b65a
YinLei <37959310+LeiLeiOfJava@users.noreply.github.com>
2020-09-05 20:14:48 +0800

    传统VideoView的使用
    需要修复的BUG是续播和每次重新创建MediaPlayer的问题、屏幕旋转的视频的纵横比

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Android滑动播放基础Demo:vhs:
 
+
+### 简要:
+
+无论是音视频播放的核心对象是MediaPlayer。
+
+除此之外，还有一个比较多用的是ExoPlayer.【Google维护】，支持Http的动态自适应流DASH、SmoothStreaming和通道加密等。
+
+然后是视图来呈现画面——SurfaceView或者Texture View。
+
+还有个控制器(音量、进度等控制)。
+
+现在还有个Media2，不过现在官方文档不太完善。
+
+### 官方提供的音视频播放的2种不同的方式：
+
+- VideoView[dev分支演示]
+- 自己实现SurfaceView和MediaPlayer
+
--- app/src/main/java/com/yinlei/viewpagervideo/MainActivity.kt
+++ app/src/main/java/com/yinlei/viewpagervideo/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.yinlei.viewpagervideo
 
+import android.media.PlaybackParams
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.MediaController
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+//https://s3.pstatp.com/aweme/resource/web/static/image/index/tvc-v3_0b9db49.mp4
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val videoPath = "android.resource://$packageName/${R.raw.meimei2}"
+        videoView.setVideoPath(videoPath)
+//        videoView.setVideoURI(Uri.parse(videoPath))
+//        videoView.setMediaController(MediaController(this))
+        videoView.setOnPreparedListener {
+            Log.d("yinlei", "mediaplay id: ${it}")
+            progressBar2.visibility = View.INVISIBLE
+            progressBar.max = it.duration
+            it.isLooping = true
+            it.playbackParams = PlaybackParams().apply {
+//                speed = 2f // 倍速播放
+//                pitch = 0.5f // 改变音高
+            }
+//            it.seekTo(5000)
+            it.start()
+        }
+        // 自定义控制器
+        lifecycleScope.launch {
+            while (true) {
+                if (videoView.isPlaying) {
+                    progressBar.progress = videoView.currentPosition
+                }
+                delay(500)
+            }
+        }
+
+//        videoView.start()
     }
 }
